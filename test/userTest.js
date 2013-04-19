8e6d671b414eff5dc4acef608c897132971c215c
Nadee Anuththara <nadee013@gmail.com>
2013-04-19 12:34:27 +0530

    added mocha mongo

--- test/userTest.js
+++ test/userTest.js
@@ -1,35 +1,47 @@
-var user = require("../lib/user");
+var User = require("../lib/user");
 var assert = require("assert");
+var mochaMongo = require("mocha-mongo");
+
+var db = mochaMongo("mongodb://localhost");
+var clean = db.cleanCollections(['users']);
 
 suite("User", function() {
 
 	suite(".register()", function() {
 
-		test("With correct data", function(done) {
+		test("With correct data", clean(function(db, done) {
 
-			user.register("arunoda", "123", function(err) {
-				assert.equal(user.data["arunoda"], "123");
-				done();
-			});
-		});
-	});
-
-	suite(".login()", function() {
-
-		test("With Correct data", function(done) {
+			// done();
+			var user = new User(db.collection('users'));
+			user.register("nadee", "123", function(err) {
 			
-			user.login("arunoda", "123", function(err, result) {
-				assert.equal(result, true);
-				done();
+				assert.ifError(err);
+				db.collection('users').findOne({_id: "nadee"}, function(err, result) {
+					// console.log(result);
+					assert.equal(result.password, "123");
+					done();
+				});
 			});
-		});
 
-		test("With incorrect data", function(done) {
-
-			user.login("nadee", "234", function(err, result) {
-				assert.equal(result, false);
-				done();	
-			});
-		});
+		}));
 	});
+
+	// suite(".login()", function() {
+
+	// 	test("With Correct data", function(done) {
+			
+	// 		user.login("arunoda", "123", function(err, result) {
+	// 			assert.equal(result, true);
+	// 			done();
+	// 		});
+	// 	});
+
+	// 	test("With incorrect data", function(done) {
+
+	// 		user.login("nadee", "234", function(err, result) {
+	// 			assert.equal(result, false);
+	// 			done();	
+	// 		});
+	// 	});
+	// });
 });
